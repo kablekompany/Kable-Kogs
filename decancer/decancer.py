6a64d608852f07447d7cdbea5fd5cae28b529b0a
PhenoM4n4n <manansingh@Santoshs-iMac-Pro.local>
2020-08-26 22:50:42 -0700

    i need slep

--- decancer/decancer.py
+++ decancer/decancer.py
@@ -19,8 +19,10 @@ class Decancer(BaseCog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=7778847744, force_registration=True,)
-        default_guild = {"modlogchannel": None, "new_custom_nick": None}
+        default_guild = {"modlogchannel": None, "new_custom_nick": "simp name", "auto": False}
+        default_global = {"auto": True}
         self.config.register_guild(**default_guild)
+        self.config.register_global(**default_global)
 
     __author__ = "KableKompany#0001"
     __version__ = "1.5.1"
@@ -29,7 +31,7 @@ class Decancer(BaseCog):
         """This cog does not store user data"""
         return
 
-    @commands.group()
+    @commands.group(invoke_without_subcommand=True)
     @checks.mod_or_permissions(manage_channels=True)
     @commands.guild_only()
     async def decancerset(self, ctx):
@@ -41,6 +43,7 @@ class Decancer(BaseCog):
             return
         channel = await self.config.guild(ctx.guild).modlogchannel()
         name = await self.config.guild(ctx.guild).new_custom_nick()
+        auto = await self.config.guild(ctx.guild).auto()
         if channel is None:
             try:
                 check_modlog_exists = await modlog.get_modlog_channel(ctx.guild)
@@ -60,7 +63,7 @@ class Decancer(BaseCog):
 
         e = discord.Embed()
         e.add_field(
-            name=f"{ctx.guild.name} Settings", value="{} `{}`".format(value_change, name),
+            name=f"{ctx.guild.name} Settings", value="{} `{}`\n**Auto-Decancer:** `{}`".format(value_change, name, auto),
         )
         e.set_footer(text="To change these, pass [p]decancerset modlog|defaultname")
         e.colour = discord.Colour.blue()
@@ -77,18 +80,9 @@ class Decancer(BaseCog):
         If you've set one up with `[p]modlogset channel`
         it will default to using that
         """
-        if not channel:
-            await ctx.send_help()
-
         channel_check = await self.config.guild(ctx.guild).modlogchannel()
-        name = await self.config.guild(ctx.guild).new_custom_nick()
 
-        if channel_check is None:
-            if not channel:
-                await ctx.send_help()
-            return
-
-        if override != "-override" and channel_check is not None:
+        if override != "-override" and channel_check:
             await ctx.send(
                 f"Your current channel is <#{channel_check}>. Pass `-override` to change this."
             )
@@ -103,17 +97,13 @@ class Decancer(BaseCog):
         await ctx.tick()
 
     @decancerset.command(aliases=["name"])
-    async def defaultname(self, ctx, *, name=None):
+    async def defaultname(self, ctx, *, name):
         """
         If you don't want a server of simps, change this
         to whatever you'd like, simp. Wrap in quotes if
         it's more than one word
             `Example: [p]decancerset name "kable is coolaf"`
         """
-        check_name = await self.config.guild(ctx.guild).new_custom_nick()
-        if check_name or name is None:
-            await ctx.send_help()
-            return
         if len(name) > 32:
             await ctx.send("Let's keep that nickname under 32 characters, scrub")
             return
@@ -122,9 +112,41 @@ class Decancer(BaseCog):
         await ctx.send(
             f"Your fallback name, should the cancer be too gd high for me to fix, is `{name}`"
         )
+    
+    @decancerset.command()
+    async def auto(self, ctx, true_or_false: bool=None):
+        """Toggle automatically decancering new users."""
+        
+        target_state = (
+            true_or_false
+            if true_or_false is not None
+            else not (await self.config.guild(ctx.guild).auto())
+        )
+        await self.config.guild(ctx.guild).auto.set(target_state)
+        if target_state:
+            await ctx.send("I will now decancer new users.")
+        else:
+            await ctx.send("I will no longer decancer new users.")
 
-    @commands.command(name="decancer")
+    @checks.is_owner()
+    @decancerset.command(name="autoglobal")
+    async def global_auto(self, ctx, true_or_false: bool=None):
+        """Enable/disable auto-decancering globally."""
+        
+        target_state = (
+            true_or_false
+            if true_or_false is not None
+            else not (await self.config.auto())
+        )
+        await self.config.auto.set(target_state)
+        if target_state:
+            await ctx.send("Automatic decancering has been re-enabled globally.")
+        else:
+            await ctx.send("Automatic decancering has been disabled globally.")
+
+    @commands.command(name="decancer", aliases=["dc"])
     @checks.mod_or_permissions(manage_nicknames=True)
+    @checks.bot_has_permissions(manage_nicknames=True)
     @commands.guild_only()
     async def nick_checker(self, ctx: commands.Context, *, user: discord.Member):
         """
@@ -135,7 +157,7 @@ class Decancer(BaseCog):
         """
         if not await self.config.guild(ctx.guild).modlogchannel():
             return await ctx.send(
-                f"Set up a modlog for this server using `{ctx.prefix}decancerset #channel`"
+                f"Set up a modlog for this server using `{ctx.prefix}decancerset modlog #channel`"
             )
         if not user:
             await ctx.send_help()
@@ -146,8 +168,6 @@ class Decancer(BaseCog):
                 if m_nick != new_cool_nick:
                     if new_cool_nick == "name_block":
                         new_cool_nick = await self.config.guild(ctx.guild).new_custom_nick()
-                        if new_cool_nick is None:
-                            new_cool_nick = "simp name"
                     try:
                         await user.edit(
                             reason=f"Old name ({m_nick}): contained special characters",
@@ -181,6 +201,46 @@ class Decancer(BaseCog):
                         await ctx.message.add_reaction("<a:Hazard_kko:695200496939565076>")
                     except Exception:
                         return
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        if member.bot:
+            return
+        guild = member.guild
+        if not(await self.config.auto() and await self.config.guild(guild).auto() and await self.config.guild(guild).modlogchannel()):
+            return
+        if not guild.me.guild_permissions.manage_nicknames:
+            await self.config.guild(guild).auto.set(False)
+            return
+        
+        old_nick = member.display_name
+        if old_nick.isascii() and old_nick.isalnum():
+            return # even though decancer output may be different than their current name, there aren't any actual cancerous characters
+
+        await asyncio.sleep(5) # waiting for auto mod actions to take place to prevent discord from fucking up the nickname edit
+        new_cool_nick = self.nick_maker(old_nick)
+        if old_nick != new_cool_nick:
+            if new_cool_nick == "name_block":
+                new_cool_nick = await self.config.guild(guild).new_custom_nick()
+            try:
+                await member.edit(reason=f"Auto Decancer | Old name ({old_nick}): contained special characters", nick=new_cool_nick)
+            except discord.errors.Forbidden:
+                await self.config.guild(guild).auto.set(False)
+            except discord.errors.NotFound:
+                pass
+            channel = guild.get_channel(await self.config.guild(guild).modlogchannel())
+            if not channel or not(channel.permissions_for(guild.me).send_messages and channel.permissions_for(guild.me).embed_links):
+                await self.config.guild(guild).modlogchannel.clear()
+                return
+            color = 0x2FFFFF
+            embed = discord.Embed(
+                color=discord.Color(color),
+                title=f"auto-decancer",
+                description=f"**Offender:** {member.name}#{member.discriminator} <@{member.id}> \n**Reason:** Remove cancerous characters from previous name\n**New Nickname:** {new_cool_nick}\n**Responsible Moderator:** {self.bot.user.name}#{self.bot.user.discriminator} <@{self.bot.user.id}>",
+                timestamp=datetime.utcnow(),
+            )
+            embed.set_footer(text=f"ID: {member.id}")
+            await channel.send(embed=embed)
 
     # the magic
     @staticmethod
