6cc3b0c624da49c2d4bb3862d76534fee4e49aac
Trent Kable <trent@kablekompany.com>
2020-10-24 23:09:48 -0500

    lets revert that for now to get parsing right

--- decancer/__init__.py
+++ decancer/__init__.py
@@ -1,3 +1,10 @@
-from .decancer import setup
+# from .dehoister import Decancer
+from .decancer import Decancer
 
-__red_end_user_data_statement__ = "This cog does not store any End User Data."
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data or metadata about users."
+)
+
+
+def setup(bot):
+    bot.add_cog(Decancer(bot))
